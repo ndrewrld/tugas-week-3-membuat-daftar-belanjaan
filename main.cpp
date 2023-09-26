e996b4a484ddd03b41065518adfdfeb94fff56fa
ndrewrld <145134087+ndrewrld@users.noreply.github.com>
2023-09-26 18:46:43 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+	cout << setw(0) << "+--+------------+--------+" << endl;
+	cout << setw(0) << "|NO|" << setw (13) << "item |" << setw(3) << " jumlah | " << endl;
+	cout << setw(0) << "+--+------------+--------+" << endl;
+	cout << setw(0) << "|01|beras " << setw(16) << "     |10kg    |" << endl;
+	cout << setw(0) << "|02|indomie" << setw(15) << "   |7bungkus|" << endl;
+	cout << setw(0) << "|03|telur " << setw(16) << "     |5butir  |" << endl;
+	cout << setw(0) << "+--+------------+--------+" << endl;
+
+
+}
